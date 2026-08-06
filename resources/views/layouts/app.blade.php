0708844eae50efde604090cff97569c1fbac823a
unknown <muhammadnaufal3864@gmail.com>
2026-08-06 11:54:22 +0700

    perbaikan bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', config('app.name', 'PRATAMA')) — {{ config('app.name') }}</title>
+        <title>@yield('title', 'PRATAMA') — {{ config('app.name', 'PRATAMA') }}</title>
 
         <meta name="description" content="@yield('metaDescription', 'PRATAMA — Prestasi dan Talenta Mahasiswa ISI Yogyakarta')">
         <meta name="robots" content="noindex, nofollow">
